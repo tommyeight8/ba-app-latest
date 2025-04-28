d19941b602784fe8f64a6eb1310524468595127b
tommy-vpr <tommy@cultivatedagency.com>
2025-04-28 16:52:43 -0700

    delete dashboard admin tasks team

--- app/dashboard/tasks/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const route = () => {
-  return <div>Tasks</div>;
-};
-
-export default route;
--- app/dashboard/team/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Team</div>;
-};
-
-export default page;
